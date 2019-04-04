4839cadcfa5e6e60b8a0abd7247aab6c77f5ac3b
Janos <kerezsi1987@gmail.com>
2019-04-04 13:41:21 +0200

    TS workshop

---
+++ week-01/day-04/hello-others.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Soma!
+// Hello, Nori!
+// Hello, Istvan!
+
+console.log("Hello, Soma!");
+console.log("Hello, Nori!");
+console.log("Hello, Istvan!");
+
