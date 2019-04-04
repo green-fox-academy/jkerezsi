4d6d5b4e7a85a5f072c932a036681ed50587bd6b
Janos <kerezsi1987@gmail.com>
2019-04-04 17:05:06 +0200

    completed exercises day4

--- week-01/day-04/seconds-in-a-day.ts
+++ week-01/day-04/seconds-in-a-day.ts
@@ -6,3 +6,8 @@ let currentSeconds: number = 42;
 
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
+
+let TotalSecondsInADay: number = (24 * (60**2));
+let PastSeconds: number = (currentHours * (60**2) + currentMinutes * 60 + 42);
+
+console.log(TotalSecondsInADay - PastSeconds);
