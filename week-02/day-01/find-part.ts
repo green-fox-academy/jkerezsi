511dc915cee94cee199e06192d9f6e4410226a14
Janos <kerezsi1987@gmail.com>
2019-05-01 14:21:56 +0200

    feat( find part function written)

---
+++ week-02/day-01/find-part.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+//  Create a function that takes a number and an array of numbers as a parameter
+//  Returns the indices of the numbers in the array of which the first number is a part of
+//  Or returns an empty list if the number is not part of any of the numbers in the array
+
+
+
+function subint(x: number, y: number[]) {
+    let emptyArr = [];
+    for (let i: number = 0; i < y.length; i++) {
+        let arr1 = y[i].toString();
+        let arr2 = x.toString();
+        if (arr1.indexOf(arr2) !== -1) {
+            emptyArr.push(i);
+        }
+    } return emptyArr;
+}
+
+
+// Example
+console.log(subint(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+console.log(subint(9, [1, 11, 34, 52, 61]));
+// should print: '[]
