0986219b8b098519adcfb9bfe1f1e1419015d86b
Janos <kerezsi1987@gmail.com>
2019-05-24 10:56:11 +0200

    added get /allbooks and new http request - finised exercise

--- week-07/day-04/booklist/app.js
+++ week-07/day-04/booklist/app.js
@@ -8,15 +8,20 @@ let mysql = require('mysql');
 const app = express();
 const PORT = 8080;
 
+//this needs to have a static home page file posted, actual file will be indicated
+//below in app.get section
 app.use(express.static('../booklist'));
+//if i post then the server is able to read json file
+app.use(express.json());
 
+//connection to database booklist
 let conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'bookinfo'
 });
-
+//database conection error handling
 conn.connect(function (err) {
   if (err) {
     console.log('Error connecting to booklist DB');
@@ -25,12 +30,24 @@ conn.connect(function (err) {
   console.log('Connection established');
 });
 
+//get request =>home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
  });
 
 app.get('/titles', (req, res) => {
   conn.query('SELECT book_name FROM book_mast;', function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.send(rows);
+  })
+});
+
+app.get('/allbooks', (req, res) => {
+  conn.query('SELECT book_mast.book_name, author.aut_name, category.cate_descrip, newpublisher.pub_name, book_mast.book_price FROM book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id INNER JOIN category ON category.cate_id = book_mast.cate_id INNER JOIN newpublisher ON newpublisher.pub_id = book_mast.pub_id;', function (err, rows) {
     if (err) {
       console.log(err.toString());
       res.status(500).send('Database error');
--- week-07/day-04/booklist/frontend.js
+++ week-07/day-04/booklist/frontend.js
@@ -1,8 +1,26 @@
 'use strict';
 
 
+    // let request = new XMLHttpRequest();
+    // request.open('GET', "http://localhost:8080/titles")
+    // request.send();
+    // request.onload = (data) => {
+    //     let parsed = JSON.parse(data.target.response);
+    //     console.log(parsed);
+    //     for (let i = 0; i < parsed.length; i++) {
+    //         let body = document.querySelector("body")
+    //         let newDiv = document.createElement("div")
+    //         newDiv.innerHTML = parsed[i].book_name;
+    //         body.appendChild(newDiv);
+    //     }
+    // };
+
+    // request.onerror = function () {
+    //     console.log('connection error');
+    // };
+
     let request = new XMLHttpRequest();
-    request.open('GET', "http://localhost:8080/titles")
+    request.open('GET', "http://localhost:8080/allbooks")
     request.send();
     request.onload = (data) => {
         let parsed = JSON.parse(data.target.response);
@@ -10,7 +28,7 @@
         for (let i = 0; i < parsed.length; i++) {
             let body = document.querySelector("body")
             let newDiv = document.createElement("div")
-            newDiv.innerHTML = parsed[i].book_name;
+            newDiv.innerHTML = `${parsed[i].book_name},${parsed[i].aut_name},${parsed[i].cate_descrip},${parsed[i].pub_name},${parsed[i].book_price}`;
             body.appendChild(newDiv);
         }
     };
@@ -18,4 +36,5 @@
     request.onerror = function () {
         console.log('connection error');
     };
+
 
